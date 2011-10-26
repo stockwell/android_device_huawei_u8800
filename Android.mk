b74a8214dcfd6ac69c2754cedc5805c89ef9099d
Matt Stockwell <matt.stockwell@optusnet.com.au>
2011-10-26 12:48:07 +1000

    Add libgralloc build (thanks dzo)

--- Android.mk
+++ Android.mk
@@ -18,11 +18,12 @@ LOCAL_PATH := $(my-dir)
 ifeq ($(TARGET_DEVICE),u8800)
     subdir_makefiles := \
         $(LOCAL_PATH)/libaudio/Android.mk \
+        $(LOCAL_PATH)/libgralloc/Android.mk \
         $(LOCAL_PATH)/liblights/Android.mk \
         $(LOCAL_PATH)/libril/Android.mk \
         $(LOCAL_PATH)/recovery/rmt_storage/Android.mk \
         $(LOCAL_PATH)/recovery/offmode/Android.mk \
-        $(LOCAL_PATH)/load_libra/Android.mk
+        $(LOCAL_PATH)/load_libra/Android.mk 
 
     include $(subdir_makefiles)
-endif
+endif # TARGET_DEVICE
